ca697d550bdb05a6d94a0271fe8aeb0857f9a03e
Andreus <Andreus@DESKTOP-I3EQQ7H>
2020-06-21 13:42:34 +0500

    Добавьте файлы проекта.

---
+++ ООП 5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ООП_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee tom = new Employee("Андрей", 22, "Apple");
+        }
+        class Person
+        {
+            string name;
+            int age;
+
+            public Person(string name)
+            {
+                this.name = name;
+                Console.WriteLine(this.name);
+            }
+            public Person(string name, int age) : this(name)
+            {
+                this.age = age;
+                Console.WriteLine(this.age);
+            }
+        }
+        class Employee : Person
+        {
+            string company;
+
+            public Employee(string name, int age, string company) : base(name, age)
+            {
+                this.company = company;
+                Console.WriteLine(name + " " +age + " года " + company);
+            }
+        }
+    }
+}
